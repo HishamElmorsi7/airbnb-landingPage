fa41fade1288c9b39614c3c4e14fc923c45ba78b
Hisham Elmorsi <hmelmorsi@gmail.com>
2024-02-27 22:56:02 +0200

    Built the card container

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,25 @@
+import bikeImg from '../assets/bike.jpeg'
+import starImg from '../assets/star.svg'
+import dotImg from '../assets/dot.svg'
 export default function Card() {
     return(
         <section className="card">
-            <img src="" />
+            <img className='card--img' src={bikeImg} />
+            <div className="card--review">
+                <img className='card--star' src={starImg} />
+                <p className='card--review--rating'>5.0</p>
+                <p className='card--review--count'>(6)</p>
+                <img className='card--review--dotImg' src={dotImg}/>
+                <p className='card--review--country'>USA</p>
+            </div>
+
+            <div className='card--title'>
+                <p>Life lessons with Katie Zaferes</p>
+            </div>
+
+            <div className='card--price'>
+                <p><span>From $136</span> / person</p>
+            </div>
         </section>
     )
 }
