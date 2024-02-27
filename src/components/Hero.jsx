ec6ba128f2baa304f99a33715bd206fe3f122f6a
Hisham Elmorsi <hmelmorsi@gmail.com>
2024-02-27 21:54:03 +0200

    Added navbar and hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import heroGrid from '../assets/hero-grid.png'
+function Hero(){
+    return (
+        <section className='hero'>
+            <img className='hero--grid' src={heroGrid} alt="college of images" />
+            <div className="hero--info">
+                <h1 className='hero--info-title'>Online Experiences</h1>
+                <p className='hero--info--description'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import airbnbLogo from '../assets/airbnb-logo.svg'
+function Nav() {
+    return (
+        <nav className="nav">
+            <img className='nav--airbnb-logo' src={airbnbLogo} alt="airbnb-logo" />
+        </nav>
+    )
+}
+export default Nav;
